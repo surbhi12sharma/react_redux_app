f4376dce90c523cdd2d3ef46c28f934e1f1b0793
surbhi_sharma <surbhi_sharma@persistent.com>
2023-10-15 09:32:40 +0530

    Set for react redux add done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1 className='text-5xl'>Welcome to React Redux</h1>
+    </>
+  );
+}
+
+export default App;
